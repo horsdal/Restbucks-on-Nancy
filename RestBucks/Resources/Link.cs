6f42796c1d06f0be3e9a9de93902341f73e38196
Christian Horsdal <c.horsdal@gmail.com>
2012-02-26 23:38:20 +0100

    a couple of tweaks to make ncrunch happy

--- RestBucks/Resources/Link.cs
+++ RestBucks/Resources/Link.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Linq;
 using System.Xml.Serialization;
-using RestBucks.Util;
 
 namespace RestBucks.Resources
 {
