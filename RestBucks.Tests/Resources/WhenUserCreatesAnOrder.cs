17d887471dbb48a317c4327b5b5b1184a351975b
Christian Horsdal <c.horsdal@gmail.com>
2012-02-29 23:44:15 +0100

    adding nancy to order creation test

--- RestBucks.Tests/Resources/WhenUserCreatesAnOrder.cs
+++ RestBucks.Tests/Resources/WhenUserCreatesAnOrder.cs
@@ -2,18 +2,20 @@ namespace RestBucks.Tests.Resources
 {
   using System;
   using System.Linq;
-  using System.Net;
 
   using Moq;
 
   using NUnit.Framework;
 
+  using Nancy;
+  using Nancy.Testing;
+
   using RestBucks.Data;
   using RestBucks.Domain;
-  using RestBucks.Infrastructure.Linking;
+  using Infrastructure.Linking;
   using RestBucks.Resources.Orders;
   using RestBucks.Resources.Orders.Representations;
-  using RestBucks.Tests.Util;
+  using Util;
 
   using SharpTestsEx;
 
@@ -36,61 +38,73 @@ namespace RestBucks.Tests.Resources
 
     private readonly IResourceLinker resourceLinker = new ResourceLinker();
 
-    public OrdersResourceHandler CreateResourceHandler(
+    public Browser CreateAppProxy(
       IRepository<Order> orderRepository = null,
       IRepository<Product> productRepository = null)
     {
       var defaultProductRepository = new RepositoryStub<Product>(
         latte, new Product {Name = "Other", Price = 3.6m});
 
-      return new OrdersResourceHandler(
-        defaultProductRepository ?? new RepositoryStub<Product>(),
-        orderRepository ?? new RepositoryStub<Order>(),
-        resourceLinker);
+      return new Browser(
+        new ConfigurableBootstrapper
+          (with =>
+           {
+             with.Dependency<IRepository<Product>>(productRepository ?? defaultProductRepository);
+             with.Dependency<IRepository<Order>>(orderRepository ?? new RepositoryStub<Order>());
+           }
+          ));
     }
 
     [Test]
     public void WhenAProductDoesNotExist_ThenReturn400AndTheProperREasonPhrase()
     {
-      var resourceHandler = CreateResourceHandler();
+      var appProxy = CreateAppProxy();
       var orderRepresentation = new OrderRepresentation
                                 {
                                   Items = {new OrderItemRepresentation {Name = "beer"}}
                                 };
 
-      var result = resourceHandler.Create(orderRepresentation);
+      var result = 
+        appProxy.Post("/orders/", with =>
+          with.Body(orderRepresentation.ToXmlString()));
 
-      result.Satisfy(rm => rm.StatusCode == HttpStatusCode.BadRequest
-                           && rm.ReasonPhrase == "We don't offer beer");
+
+      result.StatusCode.Should().Be.EqualTo(HttpStatusCode.BadRequest);
+      result.Headers["ReasonPhrase"].Should().Be.EqualTo("We don't offer beer");
     }
 
     [Test]
     public void WhenItemHasQuantity0_ThenReturn400AndTheProperREasonPhrase()
     {
-      var service = CreateResourceHandler();
+      var appProxy = CreateAppProxy();
       var orderRepresentation = new OrderRepresentation
                                 {
                                   Items = {new OrderItemRepresentation {Name = "latte", Quantity = 0}}
                                 };
+      
+      // act
+      var result =
+        appProxy.Post("/orders/", with =>
+          with.Body(orderRepresentation.ToXmlString()));
 
-      var response = service.Create(orderRepresentation);
-
-      //NOTE: I am not sure if the proper response is NotFound or BadRequest. It is not clear in the book.
-      response.Satisfy(rm => rm.StatusCode == HttpStatusCode.BadRequest
-                             && rm.Content.ToStringContent() == "Item 0: Quantity should be greater than 0.");
+      // assert
+      result.StatusCode.Should().Be.EqualTo(HttpStatusCode.BadRequest);
+      result.Headers["ReasonPhrase"].Should().Be.EqualTo("Item 0: Quantity should be greater than 0.");
     }
 
     [Test]
     public void WhenOrderIsOk_ThenInsertANewOrderWithTheProductsAndPrice()
     {
       var orderRepository = new RepositoryStub<Order>();
-      var handler = CreateResourceHandler(orderRepository);
+      var appProxy = CreateAppProxy(orderRepository);
       var orderRepresentation = new OrderRepresentation
                                 {Items = {new OrderItemRepresentation {Name = "latte", Quantity = 1}}};
 
       //act
-      handler.Create(orderRepresentation);
+      appProxy.Post("/orders/", with =>
+                    with.Body(orderRepresentation.ToXmlString()));
 
+      // assert
       var order = orderRepository.RetrieveAll().First();
       order.Satisfy(_ => _.Items.Any(i => i.Product == latte && i.UnitPrice == 2.5m && i.Quantity == 1));
     }
@@ -99,12 +113,13 @@ namespace RestBucks.Tests.Resources
     public void WhenOrderIsOk_ThenInsertANewOrderWithTheDateTime()
     {
       var orderRepository = new RepositoryStub<Order>();
-      var handler = CreateResourceHandler(orderRepository);
+      var appProxy = CreateAppProxy(orderRepository);
       var orderRepresentation = new OrderRepresentation
                                 {Items = {new OrderItemRepresentation {Name = "latte", Quantity = 1}}};
 
       //act
-      handler.Create(orderRepresentation);
+      var result = appProxy.Post("/orders/", with =>
+                                 with.Body(orderRepresentation.ToXmlString()));
 
       var order = orderRepository.RetrieveAll().First();
       order.Date.Should().Be.EqualTo(DateTime.Today);
@@ -114,7 +129,7 @@ namespace RestBucks.Tests.Resources
     public void WhenOrderIsOk_ThenInsertANewOrderWithTheLocationInfo()
     {
       var orderRepository = new RepositoryStub<Order>();
-      var handler = CreateResourceHandler(orderRepository);
+      var appProxy = CreateAppProxy(orderRepository);
       var orderRepresentation = new OrderRepresentation
                                 {
                                   Location = Location.InShop,
@@ -122,7 +137,8 @@ namespace RestBucks.Tests.Resources
                                 };
 
       //act
-      handler.Create(orderRepresentation);
+      var result = appProxy.Post("/orders/", with =>
+                                 with.Body(orderRepresentation.ToXmlString()));
 
       var order = orderRepository.RetrieveAll().First();
       order.Location.Should().Be.EqualTo(Location.InShop);
@@ -135,18 +151,21 @@ namespace RestBucks.Tests.Resources
       orderRepository.Setup(or => or.MakePersistent(It.IsAny<Order[]>()))
         .Callback<Order[]>(o => o.First().Id = 123);
 
-      var handler = CreateResourceHandler(orderRepository.Object);
-
-
-      //act
-      var result =
-        handler.Create(new OrderRepresentation {Items = {new OrderItemRepresentation {Name = "latte", Quantity = 1}}});
-
       var expectedUriToTheNewOrder =
-        resourceLinker.GetUri<OrderResourceHandler>(or => or.Get(0, null), new {orderId = "123"});
+        resourceLinker.GetUri<OrderResourceHandler>(or => or.Get(0, null), new { orderId = "123" });
 
-      result.Satisfy(r => r.StatusCode == HttpStatusCode.Created
-                          && r.Headers.Location.ToString() == expectedUriToTheNewOrder);
+      var appProxy = CreateAppProxy(orderRepository.Object);
+      var orderRepresentation = new OrderRepresentation
+                                {
+                                  Items = {new OrderItemRepresentation {Name = "latte", Quantity = 1}}
+                                };
+      // act
+      var result = appProxy.Post("/orders/", with => 
+                                 with.Body(orderRepresentation.ToXmlString()));
+
+
+      result.StatusCode.Should().Be.EqualTo(HttpStatusCode.Created);
+      result.Headers["Location"].Should().Be.EqualTo(expectedUriToTheNewOrder);
     }
   }
 }
