022bebc139fcaee9c4daf685ee775c8c68e76873
Christian Horsdal <c.horsdal@gmail.com>
2012-03-15 23:18:35 +0100

    moved test for POST /order/id/payment to Nancy.Testing. All red.

--- RestBucks.Tests/Resources/WhenUserPayAnOrder.cs
+++ RestBucks.Tests/Resources/WhenUserPayAnOrder.cs
@@ -1,36 +1,58 @@
-using System.Net;
-using Microsoft.ApplicationServer.Http;
-using NUnit.Framework;
-using RestBucks.Domain;
-using RestBucks.Infrastructure.Linking;
-using RestBucks.Resources.Orders;
-using RestBucks.Resources.Orders.Representations;
-using RestBucks.Tests.Util;
-using SharpTestsEx;
-
 namespace RestBucks.Tests.Resources
 {
-    [TestFixture]
-    public class WhenUserPayAnOrder
-    {
-       private readonly IResourceLinker resourceLinker = new ResourceLinker();
-       [Test]
-       public void WhenOrderDoesNotExist_ThenReturn404()
-       {
-           var handler = new OrderResourceHandler(new RepositoryStub<Order>(), resourceLinker);
-           var response = handler.Pay(123, new PaymentRepresentation());
-           response.StatusCode.Should().Be.EqualTo(HttpStatusCode.NotFound);
-       }
+  using NUnit.Framework;
 
-       [Test]
-       public void WhenOrderExist_ThenCancel()
-       {
-           var order = new Order{Id = 123};
-           var handler = new OrderResourceHandler(new RepositoryStub<Order>(order), resourceLinker);
-           handler.Pay(123, new PaymentRepresentation{CardNumber = "123", CardOwner = "Jose"});
-           order.Satisfy(o => o.Status == OrderStatus.Paid
-                              && o.Payment.CardOwner == "Jose"
-                              && o.Payment.CreditCardNumber == "123");
-       }
+  using Nancy;
+
+  using RestBucks.Domain;
+  using RestBucks.Resources.Orders;
+  using RestBucks.Resources.Orders.Representations;
+  using Infrastructure.Linking;
+  using Util;
+
+  using SharpTestsEx;
+
+  [TestFixture]
+  public class WhenUserPayAnOrder : ResourceHandlerTestBase
+  {
+    private readonly IResourceLinker resourceLinker = new ResourceLinker();
+
+    [Test]
+    public void WhenOrderDoesNotExist_ThenReturn404()
+    {
+      // Arrange
+      var app = CreateAppProxy();
+
+      // Act
+      var response = app.Post("/order/123/payment",
+                              with =>
+                              {
+                                with.HttpRequest();
+                                with.Body(new PaymentRepresentation().ToXmlString());
+                              });
+
+      // Assert
+      response.StatusCode.Should().Be.EqualTo(HttpStatusCode.NotFound);
     }
+
+    [Test]
+    public void WhenOrderExist_ThenCancel()
+    {
+      //Arrange
+      var order = new Order {Id = 123};
+      var app = CreateAppProxy(new RepositoryStub<Order>(order));
+
+      // Act
+      var response = app.Post("/order/123/payment/",
+                              with =>
+                              {
+                                with.HttpRequest();
+                                with.Body(new PaymentRepresentation { CardNumber = "321", CardOwner = "Jose" }.ToXmlString());
+                              });
+ 
+      order.Status.Should().Be.EqualTo(OrderStatus.Paid);
+      order.Payment.CardOwner.Should().Be.EqualTo("Jose");
+      order.Payment.CreditCardNumber.Should().Be.EqualTo("321");
+    }
+  }
 }
