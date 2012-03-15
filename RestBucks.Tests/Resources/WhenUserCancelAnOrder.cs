5d4935166a4543f0c854882ca63245639e5398b1
Christian Horsdal <c.horsdal@gmail.com>
2012-03-15 22:40:57 +0100

    moved test for DELETE /order/id to Nancy.Testing. All red since method has not been moved

--- RestBucks.Tests/Resources/WhenUserCancelAnOrder.cs
+++ RestBucks.Tests/Resources/WhenUserCancelAnOrder.cs
@@ -1,76 +1,92 @@
-using System.Net;
-using Microsoft.ApplicationServer.Http;
-using NUnit.Framework;
-using RestBucks.Domain;
-using RestBucks.Infrastructure.Linking;
-using RestBucks.Resources.Orders;
-using RestBucks.Resources.Orders.Representations;
-using RestBucks.Tests.Util;
-using SharpTestsEx;
-
 namespace RestBucks.Tests.Resources
 {
-    [TestFixture]
-    public class WhenUserCancelAnOrder
+  using NUnit.Framework;
+
+  using Nancy;
+
+  using RestBucks.Domain;
+  using RestBucks.Resources.Orders.Representations;
+
+  using Util;
+
+  using SharpTestsEx;
+
+  [TestFixture]
+  public class WhenUserCancelAnOrder : ResourceHandlerTestBase
+  {
+    [Test]
+    public void WhenOrderDoesNotExist_ThenReturn404()
     {
-        private IResourceLinker resourceLinker = new ResourceLinker();
+      // Arrange
+      var app = CreateAppProxy();
 
-        [Test]
-        public void WhenOrderDoesNotExist_ThenReturn404()
-        {
-            var handler = new OrderResourceHandler(new RepositoryStub<Order>(), resourceLinker);
-            var response = handler.Cancel(123);
-            response.StatusCode.Should().Be.EqualTo(HttpStatusCode.NotFound);
-        }
+      // Act
+      var response = app.Delete("/order/123/");
 
-        [Test]
-        public void WhenOrderExist_ThenCancel()
-        {
-            var order = new Order { Id = 123 };
-            var handler = new OrderResourceHandler(new RepositoryStub<Order>(order), resourceLinker);
-            handler.Cancel(123);
-            order.Status.Should().Be.EqualTo(OrderStatus.Canceled);
-        }
-
-        [Test]
-        public void WhenOrderExist_ThenCancelAndReturn204()
-        {
-            var order = new Order { Id = 123 };
-            var handler = new OrderResourceHandler(new RepositoryStub<Order>(order), resourceLinker);
-            var response = handler.Cancel(123);
-            response.StatusCode.Should().Be.EqualTo(HttpStatusCode.NoContent);
-        }
-
-        [Test]
-        public void ACallToGet_ShouldReturnMovedPermanentlyAndNewLocation()
-        {
-            var order = new Order { Id = 123 };
-            var handler = new OrderResourceHandler(new RepositoryStub<Order>(order), resourceLinker);
-            handler.Cancel(123);
-
-#if false
-            var responseToGet = handler.Get(123, null);
-
-            var expected = "http://restbuckson.net/trash/order/123";
-            responseToGet.Satisfy(r => r.StatusCode == HttpStatusCode.MovedPermanently
-                                    && r.Headers.Location.ToString() == expected);
-#endif
-        }
-
-        [Test]
-        public void ACallToGetCanceled_ShouldReturnTheOrder()
-        {
-            var order = new Order { Id = 123 };
-            var orderRepository = new RepositoryStub<Order>(order);
-            var handler = new OrderResourceHandler(orderRepository, resourceLinker);
-            handler.Cancel(123);
-
-            //var expected = resourceLinker.GetUri<OrderResourceHandler>(rh => rh.GetCanceled(0), new { orderId = 123 });
-            var cancelHandler = new TrashHandler(orderRepository);
-            var responseToGet = (HttpResponseMessage<OrderRepresentation>)cancelHandler.GetCanceled(123);
-
-            responseToGet.Satisfy(r => r.StatusCode == HttpStatusCode.OK
-                                    && r.Content.ReadAs() != null);
-        }
+      // Assert
+      response.StatusCode.Should().Be.EqualTo(HttpStatusCode.NotFound);
     }
+
+    [Test]
+    public void WhenOrderExist_ThenCancel()
+    {
+      // Arrange
+      var order = new Order { Id = 123 };
+      var app = CreateAppProxy(new RepositoryStub<Order>(order));
+
+      // Act
+      app.Delete("/order/123/");
+
+      // Assert
+      order.Status.Should().Be.EqualTo(OrderStatus.Canceled);
+    }
+
+    [Test]
+    public void WhenOrderExist_ThenCancelAndReturn204()
+    {
+      // Arrange
+      var order = new Order { Id = 123 };
+      var app = CreateAppProxy(new RepositoryStub<Order>(order));
+
+      // Act
+      var response = app.Delete("/order/123/");
+
+      // Assert
+      response.StatusCode.Should().Be.EqualTo(HttpStatusCode.NoContent);
+    }
+
+    [Test]
+    public void ACallToGet_ShouldReturnMovedPermanentlyAndNewLocation()
+    {
+      // Arrange
+      var order = new Order { Id = 123 };
+      var app = CreateAppProxy(new RepositoryStub<Order>(order));
+
+      // Act
+      var response = app.Delete("/order/123/");
+      var responseToGet = app.Get("/order/123/");
+
+      var expected = "http://restbuckson.net/trash/order/123";
+      responseToGet.StatusCode.Should().Be.EqualTo(HttpStatusCode.MovedPermanently);
+      responseToGet.Headers.ContainsKey("location").Should().Be.True();
+      responseToGet.Headers["location"].Should().Be.EqualTo(expected);
+    }
+
+    [Test]
+    public void ACallToGetCanceled_ShouldReturnTheOrder()
+    {
+      // Arrange
+      var order = new Order { Id = 123 };
+      var expectedBody = OrderRepresentationMapper.Map(order).ToXmlString();
+      var app = CreateAppProxy(new RepositoryStub<Order>(order));
+
+      // Act
+      var response = app.Delete("/order/123/");
+      var responseToGet = app.Get("/trash/order/123/");
+
+
+      responseToGet.StatusCode.Should().Be.EqualTo(HttpStatusCode.OK);
+      responseToGet.Body.ToXmlString().Should().Be.EqualTo(expectedBody);
+    }
+  }
 }
