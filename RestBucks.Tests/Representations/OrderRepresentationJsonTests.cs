151329b37cfe8f2074d6fb90974b83c39c32762d
Christian Horsdal <c.horsdal@gmail.com>
2012-03-18 22:01:13 +0100

    removed unused project refs and nugets in test project

--- RestBucks.Tests/Representations/OrderRepresentationJsonTests.cs
+++ RestBucks.Tests/Representations/OrderRepresentationJsonTests.cs
@@ -1,39 +1,42 @@
-using NUnit.Framework;
-using RestBucks.Domain;
-using RestBucks.Resources.Orders.Representations;
-using RestBucks.Tests.Util;
-
 namespace RestBucks.Tests.Representations
 {
-    [TestFixture]
-    public class OrderRepresentationJsonTests
+  using NUnit.Framework;
+
+  using RestBucks.Domain;
+  using RestBucks.Resources.Orders.Representations;
+
+  using Util;
+
+  [TestFixture]
+  public class OrderRepresentationJsonTests
+  {
+    [Test]
+    public void SerializeOrder()
     {
-        [Test]
-        public void SerializeOrder()
-        {
-            var orderRepresentation = new OrderRepresentation
-            {
-                Cost = 100.4m,
-                Location = Location.InShop,
-                Items =
-                                                  {
-                                                      new OrderItemRepresentation
-                                                          {
-                                                              Name = "latte", 
-                                                              Preferences =
-                                                                  {
-                                                                      { "size", "large"} ,
-                                                                      { "milk", "skim" }  
-                                                                  }
-                                                          }
-                                                  }
-            };
+      var orderRepresentation = new OrderRepresentation
+                                {
+                                  Cost = 100.4m,
+                                  Location = Location.InShop,
+                                  Items =
+                                    {
+                                      new OrderItemRepresentation
+                                      {
+                                        Name = "latte",
+                                        Preferences =
+                                          {
+                                            {"size", "large"},
+                                            {"milk", "skim"}
+                                          }
+                                      }
+                                    }
+                                };
 
-            var json = orderRepresentation.ToJsonString();
+      var json = orderRepresentation.ToJsonString();
 
-            var expected = "{\"Location\":\"InShop\",\"Cost\":100.4,\"Items\":[{\"Name\":\"latte\",\"Quantity\":0,\"Preferences\":{\"size\":\"large\",\"milk\":\"skim\"}}],\"Status\":\"OrderCreated\",\"Links\":[]}";
+      var expected =
+        "{\"Location\":\"InShop\",\"Cost\":100.4,\"Items\":[{\"Name\":\"latte\",\"Quantity\":0,\"Preferences\":{\"size\":\"large\",\"milk\":\"skim\"}}],\"Status\":\"OrderCreated\",\"Links\":[]}";
 
-            Assert.AreEqual(expected, json);
-        }
+      Assert.AreEqual(expected, json);
     }
+  }
 }
--- RestBucks.Tests/Util/JsonUtil.cs
+++ RestBucks.Tests/Util/JsonUtil.cs
@@ -1,26 +1,26 @@
-﻿using System.IO;
-using System.Runtime.Serialization.Json;
-using System.Text;
-using Newtonsoft.Json;
-
-namespace RestBucks.Tests.Util
+﻿namespace RestBucks.Tests.Util
 {
-    public static class JsonUtil
+  using System.IO;
+  using System.Text;
+
+  using Newtonsoft.Json;
+
+  public static class JsonUtil
+  {
+    public static string ToJsonString<T>(this T input)
     {
-        public static string ToJsonString<T>(this T input)
-        {
-            string xml;
-            var serializer = new JsonSerializer();
-            using (var ms = new MemoryStream())
-            using(var tw = new StreamWriter(ms))
-            using(var jswriter = new JsonTextWriter(tw))
-            {
-                serializer.Serialize(jswriter, input);
-                jswriter.Flush();
-                ms.Position = 0;
-                xml = Encoding.Default.GetString(ms.ToArray());
-            }
-            return xml;
-        }
+      string xml;
+      var serializer = new JsonSerializer();
+      using (var ms = new MemoryStream())
+      using (var tw = new StreamWriter(ms))
+      using (var jswriter = new JsonTextWriter(tw))
+      {
+        serializer.Serialize(jswriter, input);
+        jswriter.Flush();
+        ms.Position = 0;
+        xml = Encoding.Default.GetString(ms.ToArray());
+      }
+      return xml;
     }
+  }
 }
