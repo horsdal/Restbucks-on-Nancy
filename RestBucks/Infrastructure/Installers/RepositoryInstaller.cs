48d17caf85b136c28242ac47cd13e91f15340d0b
Christian Horsdal <c.horsdal@gmail.com>
2012-03-22 22:14:52 +0100

    reformatting installers

--- RestBucks/Infrastructure/Installers/RepositoryInstaller.cs
+++ RestBucks/Infrastructure/Installers/RepositoryInstaller.cs
@@ -1,16 +1,17 @@
-using Castle.MicroKernel.Registration;
-using Castle.MicroKernel.SubSystems.Configuration;
-using Castle.Windsor;
-using RestBucks.Data;
-using RestBucks.Data.Impl;
-
 namespace RestBucks.Infrastructure.Installers
 {
-    public class RepositoryInstaller : IWindsorInstaller
+  using Castle.MicroKernel.Registration;
+  using Castle.MicroKernel.SubSystems.Configuration;
+  using Castle.Windsor;
+
+  using RestBucks.Data;
+  using RestBucks.Data.Impl;
+
+  public class RepositoryInstaller : IWindsorInstaller
+  {
+    public void Install(IWindsorContainer container, IConfigurationStore store)
     {
-        public void Install(IWindsorContainer container, IConfigurationStore store)
-        {
-            container.Register(Component.For(typeof (IRepository<>)).ImplementedBy(typeof (Repository<>)));
-        }
+      container.Register(Component.For(typeof (IRepository<>)).ImplementedBy(typeof (Repository<>)));
     }
+  }
 }
