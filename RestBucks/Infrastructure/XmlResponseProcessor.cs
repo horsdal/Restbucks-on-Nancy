951230cc7c14a0b17f319963e0d2e97c78a02db3
Christian Horsdal <c.horsdal@gmail.com>
2012-09-05 09:13:06 +0200

    upgrade to Nancy 0.12, use the Nancy content negotiation api, and add an extra/custom content type

---
+++ RestBucks/Infrastructure/XmlResponseProcessor.cs
@@ -0,0 +1,62 @@
+﻿namespace RestBucks.Infrastructure
+{
+  using Nancy.Responses.Negotiation;
+  using System;
+  using System.Collections.Generic;
+  using Nancy;
+  using System.Linq;
+  using Nancy.Responses;
+
+  public class XmlResponseProcessor : IResponseProcessor
+  {
+    private ISerializer xmlSerializer;
+
+    public XmlResponseProcessor(IEnumerable<ISerializer> serializers)
+    {
+      xmlSerializer = serializers.FirstOrDefault(s => s.CanSerialize("application/xml"));
+    }
+
+    public ProcessorMatch CanProcess(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+    {
+      if (IsWildCardOrApplicationType(requestedMediaRange.Type) && IsWildCardOrXmlSubType(requestedMediaRange.Subtype))
+        return new ProcessorMatch {RequestedContentTypeResult = MatchResult.ExactMatch, ModelResult = MatchResult.DontCare};
+      else if (IsTextHhtml(requestedMediaRange))
+        return new ProcessorMatch { RequestedContentTypeResult = MatchResult.NonExactMatch, ModelResult = MatchResult.DontCare };
+      else
+        return new ProcessorMatch {ModelResult = MatchResult.NoMatch, RequestedContentTypeResult = MatchResult.NoMatch};
+    }
+
+    private bool IsTextHhtml(MediaRange requestedMediaRange)
+    {
+      return requestedMediaRange.Type.ToString().Equals("text", StringComparison.InvariantCultureIgnoreCase) &&
+             requestedMediaRange.Subtype.ToString().Equals("html", StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private static bool IsWildCardOrXmlSubType(MediaType subtype)
+    {
+      return (subtype.IsWildcard ||
+              subtype.ToString().Equals("xml", StringComparison.InvariantCultureIgnoreCase) ||
+              subtype.ToString().EndsWith("+xml", StringComparison.InvariantCultureIgnoreCase));
+    }
+
+    private static bool IsWildCardOrApplicationType(MediaType superType)
+    {
+      return (superType.IsWildcard || superType.ToString().ToLower().Equals("application"));
+    }
+
+    public Response Process(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+    {
+      var xmlResponseType = typeof (XmlResponse<>).MakeGenericType(model.GetType());
+      return Activator.CreateInstance(xmlResponseType, model, "application/vnd.restbucks+xml", xmlSerializer);
+    }
+
+    public IEnumerable<Tuple<string, MediaRange>> ExtensionMappings
+    {
+      get
+      {
+        yield return
+          new Tuple<string, MediaRange>("xml", new MediaRange {Type = "application", Subtype = "vnd.restbucks+xml"});
+      }
+    }
+  }
+}
---
+++ RestBucks/Infrastructure/YammlResponseProcessor.cs
@@ -0,0 +1,76 @@
+﻿namespace RestBucks.Infrastructure
+{
+  using System.IO;
+  using System.Yaml.Serialization;
+  using Nancy.Responses.Negotiation;
+  using System;
+  using System.Collections.Generic;
+  using Nancy;
+  using Nancy.Responses;
+
+  public class YamlWrapper : ISerializer
+  {
+    private readonly YamlSerializer serializer;
+
+    public YamlWrapper(YamlSerializer serializer)
+    {
+      this.serializer = serializer;
+    }
+
+    public bool CanSerialize(string contentType)
+    {
+      return contentType.EndsWith("yaml", StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public void Serialize<TModel>(string contentType, TModel model, Stream outputStream)
+    {
+      serializer.Serialize(outputStream, model);
+    }
+
+    public IEnumerable<string> Extensions { get { yield return "yaml"; } }
+  }
+
+
+  public class YamlResponseProcessor : IResponseProcessor
+  {
+    private ISerializer yamlSerializer;
+
+    public YamlResponseProcessor(YamlWrapper serializer)
+    {
+      yamlSerializer = serializer;
+    }
+
+    public ProcessorMatch CanProcess(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+    {
+      if (isWildCardOrApplicationType(requestedMediaRange.Type) && IsWildCardOrYamlSubType(requestedMediaRange.Subtype))
+        return new ProcessorMatch {RequestedContentTypeResult = MatchResult.ExactMatch, ModelResult = MatchResult.DontCare};
+      else
+        return new ProcessorMatch {ModelResult = MatchResult.NoMatch, RequestedContentTypeResult = MatchResult.NoMatch};
+    }
+
+    private static bool IsWildCardOrYamlSubType(MediaType subtype)
+    {
+      return (subtype.IsWildcard || subtype.ToString().ToLower().Equals("yaml") || subtype.ToString().ToLower().Contains("+yaml"));
+    }
+
+    private static bool isWildCardOrApplicationType(MediaType superType)
+    {
+      return (superType.IsWildcard || superType.ToString().ToLower().Equals("application"));
+    }
+
+    public Response Process(MediaRange requestedMediaRange, dynamic model, NancyContext context)
+    {
+      var xmlResponseType = typeof (XmlResponse<>).MakeGenericType(model.GetType());
+      return Activator.CreateInstance(xmlResponseType, model, "application/vnd.restbucks+yaml", yamlSerializer);
+    }
+
+    public IEnumerable<Tuple<string, MediaRange>> ExtensionMappings
+    {
+      get
+      {
+        yield return
+          new Tuple<string, MediaRange>("yaml", new MediaRange {Type = "application", Subtype = "vnd.restbucks+yaml"});
+      }
+    }
+  }
+}
